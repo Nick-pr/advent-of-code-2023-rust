57c439bbd0a3ac58e202cb41314549a57de2ddb1
Nick Raffaele <raffaele.nickp@gmail.com>
2023-12-18 18:55:48 -0500

    day-6: part 2 mathematical solution

--- solutions/day-6/src/lib.rs
+++ solutions/day-6/src/lib.rs
@@ -1,5 +1,6 @@
 pub use part_1::solution as part_1;
 pub use part_2::solution as part_2;
+pub use part_2::solution_mathematical as part_2_mathematical;
 
 pub const INPUT: &str = include_str!("../input");
 
@@ -81,7 +82,42 @@ mod part_2 {
                 break;
             }
         }
-
         return upper - lower + 1;
+    }
+
+    fn quadratic_formula(a: f64, b: f64, c: f64) -> (f64, f64) {
+        let t = ((-1 as f64 * b) + ((b * b) - 4 as f64 * a * c).sqrt()) / 2 as f64 * a;
+        let x = ((-1 as f64 * b) - ((b * b) - 4 as f64 * a * c).sqrt()) / 2 as f64 * a;
+
+        return (t, x);
+    }
+
+    pub fn solution_mathematical(input: &str) -> u64 {
+        let mut lines = input.lines();
+
+        let time = lines
+            .next()
+            .unwrap()
+            .split_once(":")
+            .unwrap()
+            .1
+            .split_whitespace()
+            .collect::<String>()
+            .parse::<f64>()
+            .unwrap();
+
+        let distance = lines
+            .next()
+            .unwrap()
+            .split_once(":")
+            .unwrap()
+            .1
+            .split_whitespace()
+            .collect::<String>()
+            .parse::<f64>()
+            .unwrap();
+
+        let (lower, upper) = quadratic_formula(-1_f64, time, distance * -1_f64);
+        return (upper - lower) as u64 + 1;
     }
 }
--- solutions/day-6/tests/solutions.rs
+++ solutions/day-6/tests/solutions.rs
@@ -7,3 +7,8 @@ fn test_part_1() {
 fn test_part_2() {
     assert_eq!(day_6::part_2(day_6::INPUT), 30565288);
 }
+
+#[test]
+fn test_part_2_mathmatical() {
+    assert_eq!(day_6::part_2_mathematical(day_6::INPUT), 30565288);
+}
