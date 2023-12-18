49ea61080dad03fcb72f842155484dc409b3caf6
Nick Raffaele <raffaele.nickp@gmail.com>
2023-12-18 18:04:32 -0500

    day-6: initial solution

--- solutions/day-6/src/lib.rs
+++ solutions/day-6/src/lib.rs
@@ -1,5 +1,5 @@
 pub use part_1::solution as part_1;
-// pub use part_2::solution as part_2;
+pub use part_2::solution as part_2;
 
 pub const INPUT: &str = include_str!("../input");
 
@@ -40,6 +40,48 @@ mod part_1 {
     }
 }
 
-// mod part_2 {
-//     pub fn solution(input: &str) -> u32 {}
-// }
+mod part_2 {
+    pub fn solution(input: &str) -> u64 {
+        let mut lines = input.lines();
+
+        let time = lines
+            .next()
+            .unwrap()
+            .split_once(":")
+            .unwrap()
+            .1
+            .split_whitespace()
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap();
+
+        let distance = lines
+            .next()
+            .unwrap()
+            .split_once(":")
+            .unwrap()
+            .1
+            .split_whitespace()
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap();
+
+        let mut lower: u64 = 0;
+        for t in 0..=time {
+            if t * (time - t) >= distance {
+                lower = t;
+                break;
+            }
+        }
+
+        let mut upper: u64 = 0;
+        for t in (0..=time).into_iter().rev() {
+            if t * (time - t) >= distance {
+                upper = t;
+                break;
+            }
+        }
+
+        return upper - lower + 1;
+    }
+}
--- solutions/day-6/tests/solutions.rs
+++ solutions/day-6/tests/solutions.rs
@@ -3,7 +3,7 @@ fn test_part_1() {
     assert_eq!(day_6::part_1(day_6::INPUT), 1413720);
 }
 
-// #[test]
-// fn test_part_2() {
-//     assert_eq!(day_6::part_2(day_6::INPUT), 11554135);
-// }
+#[test]
+fn test_part_2() {
+    assert_eq!(day_6::part_2(day_6::INPUT), 30565288);
+}
