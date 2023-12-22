362ea53b7ad4f251fe5c1ca4842fe4905f6ac2ee
Nick Raffaele <raffaele.nickp@gmail.com>
2023-12-22 17:24:55 -0500

    day-9: part 2 initial solution

--- solutions/day-9/src/part_2.rs
+++ solutions/day-9/src/part_2.rs
@@ -1,3 +1,19 @@
-pub fn solution(_input: &str) -> u64 {
-    return 0
+use super::parse_input;
+
+pub fn solution(input: &str) -> i64 {
+    let history = parse_input(input);
+    return history
+        .into_iter()
+        .map(|line| extrapolate_previous_value(line))
+        .sum();
+}
+
+fn extrapolate_previous_value(line: Vec<i64>) -> i64 {
+    if line.iter().all(|x| *x == 0) {
+        return 0;
+    };
+
+    let diffs: Vec<i64> = line.windows(2).map(|x| x[1] - x[0]).collect();
+
+    return line.first().unwrap() - extrapolate_previous_value(diffs);
 }
--- solutions/day-9/tests/solutions.rs
+++ solutions/day-9/tests/solutions.rs
@@ -3,7 +3,7 @@ fn test_part_1() {
     assert_eq!(day_9::part_1(day_9::INPUT), 1939607039);
 }
 
-// #[test]
-// fn test_part_2() {
-//     assert_eq!(day_8::part_2(day_8::INPUT), 18024643846273);
-// }
+#[test]
+fn test_part_2() {
+    assert_eq!(day_9::part_2(day_9::INPUT), 1041);
+}
