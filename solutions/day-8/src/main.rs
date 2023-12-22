3ae84becc41e610860151551a5ff1ce46527cbed
Nick Raffaele <raffaele.nickp@gmail.com>
2023-12-22 13:53:23 -0500

    day-8: part 2 initial solution

--- solutions/day-8/src/main.rs
+++ solutions/day-8/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("------- Day 8 -------");
     println!("Part 1: {}", day_8::part_1(day_8::INPUT));
-    // println!("Part 2: {}", day_8::part_2(day_8::INPUT));
+    println!("Part 2: {}", day_8::part_2(day_8::INPUT));
 }
--- solutions/day-8/src/part_2.rs
+++ solutions/day-8/src/part_2.rs
@@ -1,3 +1,38 @@
+use crate::{parse_input, Direction};
+use num::integer::lcm;
+
 pub fn solution(input: &str) -> u64 {
-    return 0
+    let (directions, nodes) = parse_input(input);
+
+    let steps: Vec<u64> = nodes
+        .keys()
+        .filter(|node| node.ends_with("A"))
+        .map(|node| {
+            let mut steps: u64 = 0;
+            let mut current = *node;
+
+            while !current.ends_with("Z") {
+                for direction in directions.iter() {
+                    let (left, right) = nodes.get(current).unwrap();
+                    current = match direction {
+                        Direction::Right => right,
+                        Direction::Left => left,
+                    };
+                    steps += 1;
+                }
+            }
+            steps
+        })
+        .collect();
+
+    return lcm_vec(steps);
+}
+
+fn lcm_vec(nums: Vec<u64>) -> u64 {
+    let mut result: u64 = lcm(nums[0], nums[1]);
+
+    for num in &nums[2..] {
+        result = lcm(result, *num);
+    }
+    return result;
 }
